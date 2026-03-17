706b424505d4f5a7181f09590229f197c166d802
elvladi15 <vladimirfranciscodiaz14@hotmail.com>
2026-03-17 15:38:50 -0400

    create .gitignore and delete excel file

--- misc/generate_exchange_rates.py
+++ misc/generate_exchange_rates.py
@@ -1,6 +1,6 @@
 import os
 from misc.utils import generate_exchange_rates
 
-os.makedirs('project/UNCLEANED_CURRENCY_EXCHANGE_RATES', exist_ok=True)
-
-generate_exchange_rates()
+if not os.path.isdir('project/UNCLEANED_CURRENCY_EXCHANGE_RATES'):
+	os.makedirs('project/UNCLEANED_CURRENCY_EXCHANGE_RATES')
+	generate_exchange_rates()
