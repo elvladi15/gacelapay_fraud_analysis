60dda104a55a7ac0eb6674946754894d6bec37d1
elvladi15 <vladimirfranciscodiaz14@hotmail.com>
2026-03-25 15:54:08 -0400

    Finish uncleaning and cleaning datasets. Issues log updated

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
 from project.utils import generate_csv_file_from_sql
 
-
-#generate_csv_file_from_sql('project/SQL/CLEAN_EXCHANGE_RATES.sql', 'project/CLEAN_DATASETS/CLEAN_COMBINED_EXCHANGE_RATES.csv')
-#generate_csv_file_from_sql('project/SQL/CLEAN_ACCOUNTS.sql', 'project/CLEAN_DATASETS/ACCOUNTS.csv')
-#generate_csv_file_from_sql('project/SQL/CLEAN_CUSTOMERS.sql', 'project/CLEAN_DATASETS/CUSTOMERS.csv')
+generate_csv_file_from_sql('project/SQL/CLEAN_EXCHANGE_RATES.sql', 'project/CLEAN_DATASETS/CLEAN_COMBINED_EXCHANGE_RATES.csv')
+generate_csv_file_from_sql('project/SQL/CLEAN_ACCOUNTS.sql', 'project/CLEAN_DATASETS/ACCOUNTS.csv')
+generate_csv_file_from_sql('project/SQL/CLEAN_CUSTOMERS.sql', 'project/CLEAN_DATASETS/CUSTOMERS.csv')
+generate_csv_file_from_sql('project/SQL/CLEAN_FRAUD_DECISIONS.sql', 'project/CLEAN_DATASETS/FRAUD_DECISIONS.csv')
 generate_csv_file_from_sql('project/SQL/CLEAN_TRANSACTIONS.sql', 'project/CLEAN_DATASETS/TRANSACTIONS.csv')
-
-#generate_csv_file_from_sql('project/SQL/CLEAN_FRAUD_DECISIONS.sql', 'project/CLEAN_DATASETS/FRAUD_DECISIONS.csv')
+generate_csv_file_from_sql('project/SQL/CLEAN_FRAUD_COSTS.sql', 'project/CLEAN_DATASETS/FRAUD_COSTS.csv')
