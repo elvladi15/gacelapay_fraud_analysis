fefd11768f9474ae2c5e93c5ad6cba8764424732
elvladi15 <vladimirfranciscodiaz14@hotmail.com>
2026-04-06 17:42:34 -0400

    finished test case algorithm

--- project/ANALYSIS/analyze_transactions_data.py
+++ project/ANALYSIS/analyze_transactions_data.py
@@ -1,6 +1,8 @@
 import duckdb
 from scipy.special import logit, expit
 import project.utils
+import pandas as pd
+import math
 
 def get_transaction_statistics_df(weights, threshold):
 	threshold_probability = threshold / 1000
@@ -13,15 +15,15 @@ def get_transaction_statistics_df(weights, threshold):
 
 			logit_value = logit(ml_probability)
 
-			# feature 1
+			 #feature 1
 			if joined_tables_for_analysis.at[row.Index, 'TIME'] > '23:00:00':
 				logit_value += weights[0]
 
-			# feature 2
+			 #feature 2
 			if joined_tables_for_analysis.at[row.Index, 'CUSTOMER_COUNTRY'] == 'Brazil':
 				logit_value += weights[1]
 
-			# feature 3
+			 #feature 3
 			if joined_tables_for_analysis.at[row.Index, 'TRANSACTION_USD_AMOUNT'] > 10000:
 				logit_value += weights[2]
 
@@ -32,6 +34,7 @@ def get_transaction_statistics_df(weights, threshold):
 	fraud_statistics_df.at[0, 'W1'] = weights[0]
 	fraud_statistics_df.at[0, 'W2'] = weights[1]
 	fraud_statistics_df.at[0, 'W3'] = weights[2]
+	fraud_statistics_df.at[0, 'THRESHOLD'] = threshold
 
 	fraud_statistics_df = fraud_statistics_df.reindex(columns=
 	[
@@ -39,6 +42,7 @@ def get_transaction_statistics_df(weights, threshold):
 		'W1',
 		'W2',
 		'W3',
+		'THRESHOLD',
 		'FALSE_POSITIVE_RATE',
 		'GRAND_TOTAL',
 		'FRAUD_DETECTION_RATE',
@@ -53,21 +57,94 @@ def get_transaction_statistics_df(weights, threshold):
 
 	return fraud_statistics_df
 
+def get_weights_list_for_parameter(weight_details):
+	output_list = []
+	for i in range(2 * weight_details['quantity'] + 1):
+		output_list.append((i - weight_details['quantity']) * weight_details['steps'] + weight_details['value'])
+	
+	return output_list
 
 def create_fraud_statistics_csv():
+	file_name = 'project/ANALYSIS/FRAUD_STATISTICS.csv'
 
-	for i in range(3):
-		match i:
-			case 0:
-				df = get_transaction_statistics_df([0, 0, 0], 700) #Default case
-			case 1:
-				df = get_transaction_statistics_df([0.5, 0.6, 0.7], 700)
-			case 2:
-				df = get_transaction_statistics_df([0.8, 0.9, 1], 700)
+	weight_list = []
+
+	weight_parameters = [
+		{
+			'value': -6.57343924,
+			'steps': 0.5,
+			'quantity': 3
+		},
+		{
+			'value': 1.9844875,
+			'steps': 0.5,
+			'quantity': 3
+		},
+		{
+			'value': 2.9017253,
+			'steps': 0.5,
+			'quantity': 3
+		},
+	]
+
+	threshold_parameters = {
+		'value': 700,
+		'steps': 50,
+		'quantity': 3
+	}
+
+	for weight_parameter in weight_parameters:
+		weight_list.append(get_weights_list_for_parameter(weight_parameter))
+
+	threshold_list = get_weights_list_for_parameter(threshold_parameters)
+
+	weight_list.append(threshold_list)
+
+	total_cases_quantity = 1
+	list_weights_for_iteration = []
+
+	for list in weight_list:
+		total_cases_quantity *= len(list)
+
+	weight = total_cases_quantity
+
+	for list in weight_list:
+		weight /= len(list)
+
+		list_weights_for_iteration.append({'Count': len(list), 'Weight': weight})
+
+	#all_indexes_list = []
+
+	for i in range(total_cases_quantity):
+		index_list = []
+		
+		remnant = i
+
+		for item in list_weights_for_iteration:
+			index = math.floor(remnant /item['Weight'])
+
+			index_list.append(index)
+
+			remnant -= index * item['Weight']
+		
+		final_weight_list = []
+
+		for index, item in enumerate(index_list[:-1]):
+			final_weight_list.append(weight_list[index][item])
+
+		final_threshold = threshold_list[index_list[-1]]
+
+		df = get_transaction_statistics_df(final_weight_list, final_threshold)
 
 		if i == 0:
-			df.to_csv('project/ANALYSIS/FRAUD_STATISTICS.csv', index=False)
+			df.to_csv(file_name, index=False)
 		else:
-			df.to_csv('project/ANALYSIS/FRAUD_STATISTICS.csv', mode='a', index=False, header=False)
+			df.to_csv(file_name, mode='a', index=False, header=False)
+
+	sorted_df = pd.read_csv(file_name)
+
+	sorted_df = sorted_df.sort_values(by=['FALSE_POSITIVE_RATE', 'GRAND_TOTAL'], ascending=[True, False])
+
+	sorted_df.to_csv(file_name, index=False)
 
 create_fraud_statistics_csv()
